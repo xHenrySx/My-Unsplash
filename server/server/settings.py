c0a462fe3018ea1f739922f69042d044bf996712
xHenrySx <enrisal2000@gmail.com>
2023-02-18 15:04:16 -0300

    Ready for render

--- server/server/settings.py
+++ server/server/settings.py
@@ -138,11 +138,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, '../client/build/static')
-# ]
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
